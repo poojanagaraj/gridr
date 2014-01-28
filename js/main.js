8ec71f1f69a0c68db09dbae32d86379c39ecd9d1
OpenShift guest <pooja32323@gmail.com>
2014-01-27 21:57:45 -0500

    Using store in main.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 Ext.onReady(function() {
-   Ext.Msg.alert('Welcome!', 'Welcome!');
+  // Ext.Msg.alert('Welcome!', 'Welcome!');
+  
+  // 2. Create a store from data/risk.json
+  var store = new Ext.data.JsonStore({
+    url: 'data/risk.json',
+    storeId: 'riskStore',
+    root: 'risks',
+    idProperty: 'id',
+    fields: ['id', 'url', {name:'size', type: 'float'}, {name:'lastmod', type:'date'}]
+});
 });
