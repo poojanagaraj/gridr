47897948cd914eb7124ba3da30e704e06178361f
Rishabh Rao <rishabhsrao@gmail.com>
2014-01-28 09:20:49 +0530

    Creating dummy data.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 Ext.onReady(function() {
-  // Ext.Msg.alert('Welcome!', 'Welcome!');
+  // Ext.Msg.alert("Welcome!", "Welcome!");
   
-  // 2. Create a store from data/risk.json
+  // 1. Create JSON data at data/risks.json
+  // 2. Create a store from data/risks.json
   var store = new Ext.data.JsonStore({
-    url: 'data/risk.json',
-    storeId: 'riskStore',
-    root: 'risks',
-    idProperty: 'id',
-    fields: ['id', 'url', {name:'size', type: 'float'}, {name:'lastmod', type:'date'}]
+    url: "data/risks.json",
+    storeId: "riskStore",
+    root: "risks",
+    idProperty: "id",
+    fields: ["id", ", "url", {name:"size", type: "float"}, {name:"lastmod", type:"date"}]
 });
 });
